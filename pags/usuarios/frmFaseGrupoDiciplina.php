9c9b312286b2458b27603681289af5bc91f18772
Giovanni Romero <ggabrielrd@gmail.com>
2017-08-06 17:07:21 -0500

    cambios

--- pags/usuarios/frmFaseGrupoDiciplina.php
+++ pags/usuarios/frmFaseGrupoDiciplina.php
@@ -8,23 +8,48 @@ conectarse();
 $id = $_POST['id'];
 $idCampeonato = $_POST['idCampeonato'];
 
-$html = "<ul uk-tab>";
-	$html .= "<li><a href='#'>HOMBRES</a></li>";
-	$html .= "<li><a href='#'>MUJERES</a></li>";
-$html .= "</ul>";
+$html = '<div class="tabbable">
+					<ul class="nav nav-tabs padding-12 tab-color-blue background-blue">';
+	$html .= '<li>
+							<a data-toggle="tab" href="#acorHombres">Hombres</a>
+						</li>';
+	$html .= '<li>
+							<a data-toggle="tab" href="#acorMujeres">Mujeres</a>
+						</li>';
+$html .= '</ul>
+				</div>';
 
-$html .= "<ul class='uk-switcher uk-margin'>";
-$html .= "<li>";
+$html .= '<div class="tab-content">';
+$html .= '<div id="acorHombres" class="tab-pane">
+						<div id="accordion" class="accordion-style1 panel-group accordion-style2">';
 
 $resultadoVarones=getEquiposCampeonato($idCampeonato, $id,"true");
-	$html .= '<ul uk-accordion>';
 	while($filaVarones=pg_fetch_array($resultadoVarones)){
 		$idequipo = $filaVarones['id_equipo'];
 		$equipo = $filaVarones['equipo'];
-		$html .= "<li>";
-		$html .= '<h3 class="uk-accordion-title uk-text-left">'.$equipo.'</h3>';
-		$html .= '<div class="uk-accordion-content"><table class="uk-table uk-table-divider"><tbody>';
-		
+
+		$html .= '<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#'.$idequipo.'h" aria-expanded="false">
+											<i class="bigger-110 ace-icon fa fa-angle-right" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>
+											&nbsp;'.$equipo.'
+										</a>
+									</h4>
+								</div>
+
+							<div class="panel-collapse collapse" id="'.$idequipo.'h" aria-expanded="false" style="height: 0px;">
+								<div class="panel-body">
+									<table id="simple-table" class="table  table-bordered table-hover">
+									<thead>
+										<tr>
+											<th>Cédula</th>
+											<th>Nombres</th>
+											<th>Nro de Camiseta</th>
+										</tr>
+									</thead>
+										<tbody>';
+
 		$resultadoEquiposV=getSeleccionados($idequipo);
 		while($filaEquiposV=pg_fetch_array($resultadoEquiposV)){
 			$nombre =  $filaEquiposV['razon_social'];
@@ -36,35 +61,66 @@ $resultadoVarones=getEquiposCampeonato($idCampeonato, $id,"true");
 						<td>'.$num_camiseta.'</td>
 					  </tr>';
 		}
-		$html .= '</tbody></table></div></li>';
-		
-	}
-	$html .= '</ul></li>';
+		$html .= '</tbody>
+						</table>
+					</div>
+				</div>
+			</div>';
 
-$html .= "<li>";
+	}
+	$html .= '</div>
+					</div>';
+
+$html .= '<div id="acorMujeres" class="tab-pane">
+						<div id="accordion" class="accordion-style1 panel-group accordion-style2">';
 $resultadoMujeres=getEquiposCampeonato($idCampeonato, $id,"false");
-	$html .= '<ul uk-accordion>';
 	while($filaMujeres=pg_fetch_array($resultadoMujeres)){
+		$idequipo = $filaMujeres['id_equipo'];
 		$equipo = $filaMujeres['equipo'];
-		$html .= "<li>";
-		$html .= '<h3 class="uk-accordion-title uk-text-left">'.$equipo.'</h3>';
-		$html .= '<div class="uk-accordion-content">';
-		
+		$html .= '<div class="panel panel-default">
+								<div class="panel-heading">
+									<h4 class="panel-title">
+										<a class="accordion-toggle collapsed" data-toggle="collapse" data-parent="#accordion" href="#'.$idequipo.'m" aria-expanded="false">
+											<i class="bigger-110 ace-icon fa fa-angle-right" data-icon-hide="ace-icon fa fa-angle-down" data-icon-show="ace-icon fa fa-angle-right"></i>
+											&nbsp;'.$equipo.'
+										</a>
+									</h4>
+								</div>
+
+							<div class="panel-collapse collapse" id="'.$idequipo.'m" aria-expanded="false" style="height: 0px;">
+								<div class="panel-body">
+									<table id="simple-table" class="table  table-bordered table-hover">
+									<thead>
+										<tr>
+											<th>Cédula</th>
+											<th>Nombres</th>
+											<th>Nro de Camiseta</th>
+										</tr>
+									</thead>
+										<tbody>';
+
 		$resultadoEquiposM=getSeleccionados($idequipo);
 		while($filaEquiposM=pg_fetch_array($resultadoEquiposM)){
 			$nombre =  $filaEquiposM['razon_social'];
 			$cedula =  $filaEquiposM['cedula'];
 			$num_camiseta =  $filaEquiposM['num_camiseta'];
-			$html .= '<hr class="uk-divider-icon"><div class="uk-column-1-1@s uk-column-1-2@m uk-column-1-3@l">
-						<p class="uk-text-left">'.$cedula.'</p>
-						<p class="uk-text-left">'.$nombre.'</p>
-						<p>'.$num_camiseta.'</p>
-					  </div>';
+			$html .= '<tr>
+						<td>'.$cedula.'</td>
+						<td>'.$nombre.'</td>
+						<td>'.$num_camiseta.'</td>
+					  </tr>';
 		}
-		$html .= '</div></li>';
+		$html .= '</tbody>
+						</table>
+					</div>
+				</div>
+			</div>';
 	}
 
-$html .= "</ul></li></ul>";
+	$html .= '</div>
+					</div>';
+
+$html .= '</div>';
 echo $html;
 pg_close();
 ?>
